13cff71c9819e87ce93c6d9627c5bef8982a3611
MrGeoTech <mrgeotech@mrgeotech.net>
2024-09-11 10:45:44 -0500

    Add ECE 111 and more 376 homework

---
+++ ECE 111/Homeworks/HW01.m
@@ -0,0 +1,6 @@
+X = [-10:1.0:18];
+Y = 3 .* X + 5;
+plot(X, Y);
+xlabel("x");
+ylabel("y");
+title("Homework 1, Problem 2");
--- ECE 376/Projects/test/test.asm
+++ ECE 376/Projects/test/test.asm
@@ -8,10 +8,35 @@
     movlw 0x0F
     movwf ADCON1
 
-    movf   B, W
-    cpfsgt A
-    bra    2
-    incf   C, F
-    cpfslt A
-    bra    2
-    decf   C, F
+Loop1:
+    movlw  0
+    cpfseq PORTB
+    goto   Loop1
+Loop2:
+    btfsc  PORTB, 0
+    goto   Six
+    btfsc  PORTB, 1
+    goto   Eight
+Sixteen:
+    incf   PORTC, F  ; Increment
+    movlw  16
+    cpfslt PORTC
+    goto   Loop1      ; Less than 16
+    subwf  PORTC, F   ; More than 16
+    goto   Loop1
+Six:
+    incf   PORTC, F  ; Increment
+    movlw  6
+    cpfslt PORTC
+    goto   Loop1      ; Less than 6
+    subwf  PORTC, F   ; More than 6
+    goto   Loop1
+Eight:
+    incf   PORTC, F  ; Increment
+    movlw  8
+    cpfslt PORTC
+    goto   Loop1      ; Less than 8
+    subwf  PORTC, F   ; More than 8
+    goto   Loop1
+
+    end
