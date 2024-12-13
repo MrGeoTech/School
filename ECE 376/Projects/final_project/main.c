5de6dd608356d88cab6557da2dc405e8c28b8aec
MrGeoTech <mrgeotech@mrgeotech.net>
2024-12-13 17:00:08 -0600

    Fixed and made main.c work

--- ECE 376/Projects/final_project/main.c
+++ ECE 376/Projects/final_project/main.c
@@ -4,46 +4,37 @@ u8_t current_hr = 0;
 u8_t display_on = 1;
 
 void incTemp(u16_t amount) {
-    reverse_full(&PORTC, 200 * amount, 0);
+    reverse_full(&PORTC, 20 * amount, 0);
 }
 
 void decTemp(u16_t amount) {
-    rotate_full(&PORTC, 200 * amount, 0);
+    rotate_full(&PORTC, 20 * amount, 0);
 }
 
 void resetTMR0() {
-    TMR0H = 0x92; // 2^24 - 28125 = 16749091 = 0x9223
-    TMR0L = 0x23;
-}
-
-void incrementCurrentHR() {
-    current_hr++;
-
-    if (current_hr >= 24) {
-        current_hr = 0;
-    } else if (current_hr == 6) {
-        incTemp(15);
-    } else if (current_hr == 9) {
-        decTemp(3);
-    } else if (current_hr == 22) {
-        decTemp(12);
-    }
+    TMR0H = 0xFE; // 2^16 - 1000 = 0xFC18
+    TMR0L = 0x18;
+    //TMR0H = 0x92; // 2^16 - 28125 = 37411 = 0x9223
+    //TMR0L = 0x23;
 }
 
 void __interrupt() int_handler() {
     if (TMR2IF) {
-        PORTB = 0x10;
-        PORTB = 0x00;
+        // Toggle RB4
+        PORTB ^= 0x10;
+        wait_fractional(2);
+        PORTB ^= 0x10;
         TMR2IF = 0;
     } else if (TMR0IF) {
-        incrementCurrentHR();
+        current_hr++;
+        resetTMR0();
         TMR0IF = 0;
     }
 }
 
 // App that controls my thermostat based on time.
 int main() {
-    TRISA = 0x00;
+    TRISA = 0x10;
     TRISB = 0xE0;
     TRISC = 0xFF;
 
@@ -54,31 +45,48 @@ int main() {
     TMR2IP = 1;
 
     // MS -> HR
-    T0COM = 0xAE; // Enable, 16-bit, Pin Triggered, Prescaller Assigned, 1:128
+    T0CON = 0xAE; // Enable, 16-bit, Pin Triggered, Prescaller Assigned, 1:128
     resetTMR0();
     TMR0IE = 1;
     TMR0IP = 1;
 
-    PIE = 1;
+    PEIE = 1;
     GIE = 1;
+
+    u8_t prev_hr = current_hr - 1;
+    lcd_init();
+    lcd_append_all("Current hour: ");
 
     while (1) {
         if (PORTB & (1 << 7)) { // RB7
             display_on = !display_on;
+            lcd_send(CLEAR);
+            if (display_on) {
+                lcd_send(CONTROL_ON_NOCURSOR);
+                lcd_append_all("Current hour: ");
+            } else {
+                lcd_send(CONTROL_OFF_NOCURSOR);
+            }
             while (PORTB & 0x80);
-            wait(1);
+            wait(100); // Poor mans debouncer
         }
 
         // Send full instructions incase an interrupt happens mid-instruction
-        if (display_on) {
-            lcd_send(CLEAR);
-            lcd_send(CONTROL_ON_NOCURSOR);
-            lcd_append_all("Current hour: ");
+        if (display_on && prev_hr != current_hr) {
             lcd_goto(1, 0);
             lcd_append_int(current_hr, 2, 0);
-        } else {
-            lcd_send(CLEAR);
-            lcd_send(CONTROL_OFF_NOCURSOR);
+            prev_hr = current_hr;
+
+            // Move motor "asynchronous" to the timing
+            if (current_hr >= 24) {
+                current_hr = 0;
+            } else if (current_hr == 6) {
+                incTemp(15);
+            } else if (current_hr == 9) {
+                decTemp(3);
+            } else if (current_hr == 22) {
+                decTemp(12);
+            }
         }
 
         if (PORTB & (1 << 6)) { // RB6
@@ -86,13 +94,15 @@ int main() {
             TMR2 = 0;
             resetTMR0();
             while (PORTB & 0x40);
-            wait(1);
+            wait(100); // Poor mans debouncer
         }
 
         if (PORTB & (1 << 5)) { // RB5
-            incrementCurrentHR();
-            while (PORTB & 0x40);
-            wait(1);
+            current_hr++;
+            while (PORTB & 0x20);
+            wait(100); // Poor mans debouncer
         }
+
+        wait(1);
     }
 }
--- ECE 376/Projects/testc/main.c
+++ ECE 376/Projects/testc/main.c
@@ -1,73 +1,108 @@
-#pragma warning disable 520,1385,1311,1510,1518,2053
 #include "piclib.h"
 
-#define E4 15169
-#define E4_LOW 15184
-#define F4 14317
+u8_t current_hr = 0;
+u8_t display_on = 1;
 
-u8_t playing;
-u16_t note;
+void incTemp(u16_t amount) {
+    reverse_full(&PORTC, 20 * amount, 0);
+}
 
-void __interrupt() interrupt_handler() {
-    if (CCP1IF) {
-        if (playing) RC2 ^= 1;
-        CCPR1 += note;
-        CCP1IF = 0;
+void decTemp(u16_t amount) {
+    rotate_full(&PORTC, 20 * amount, 0);
+}
+
+void resetTMR0() {
+    TMR0H = 0xFE; // 2^16 - 1000 = 0xFC18
+    TMR0L = 0x18;
+    //TMR0H = 0x92; // 2^16 - 28125 = 37411 = 0x9223
+    //TMR0L = 0x23;
+}
+
+void __interrupt() int_handler() {
+    if (TMR2IF) {
+        // Toggle RB4
+        PORTB ^= 0x10;
+        wait_fractional(2);
+        PORTB ^= 0x10;
+        TMR2IF = 0;
+    } else if (TMR0IF) {
+        current_hr++;
+        resetTMR0();
+        TMR0IF = 0;
     }
 }
 
-void play(u16_t new_note, u16_t milliseconds) {
-    note = new_note;
-    playing = 1;
-    wait(milliseconds);
-    playing = 0;
-}
-
+// App that controls my thermostat based on time.
 int main() {
-    TRISB = 0x03;
-    PORTB = 0x00;
+    TRISA = 0x10;
+    TRISB = 0xE0;
+    TRISC = 0xFF;
 
-    TRISC = 0x00;
-    PORTC = 0x00;
+    // CLOCK -> MS
+    T2CON = 0xFF; // 1:16, Enable, PS = 5
+    PR2 = 125; // 16 * 5 * 125 = 10,000
+    TMR2IE = 1;
+    TMR2IP = 1;
 
-    TMR1CS = 0;
-    T1CON = 0x81;
-    TMR1ON = 1;
-    TMR1IE = 1;
-    TMR1IP = 1;
+    // MS -> HR
+    T0CON = 0xAE; // Enable, 16-bit, Pin Triggered, Prescaller Assigned, 1:128
+    resetTMR0();
+    TMR0IE = 1;
+    TMR0IP = 1;
 
-    CCP1CON = 0x0A;
-    CCP1IE = 1;
-    CCPR1 = 0;
+    PEIE = 1;
+    GIE = 1;
 
-    timers_enable();
-    u8_t right = 0;
-    u8_t wrong = 0;
+    u8_t prev_hr = current_hr - 1;
+    lcd_init();
+    lcd_append_all("Current hour: ");
+
     while (1) {
-        while (!RB0);
-        while (RB0);
+        if (PORTB & (1 << 7)) { // RB7
+            display_on = !display_on;
+            lcd_send(CLEAR);
+            if (display_on) {
+                lcd_send(CONTROL_ON_NOCURSOR);
+                lcd_append_all("Current hour: ");
+            } else {
+                lcd_send(CONTROL_OFF_NOCURSOR);
+            }
+            while (PORTB & 0x80);
+            wait(100); // Poor mans debouncer
+        }
 
-        u8_t is_same = TMR1 & 1;
+        // Send full instructions incase an interrupt happens mid-instruction
+        if (display_on && prev_hr != current_hr) {
+            lcd_goto(1, 0);
+            lcd_append_int(current_hr, 2, 0);
+            prev_hr = current_hr;
 
-        play(E4, 500);
-        wait(200);
-        if (is_same)
-            play(E4, 500);
-        else
-            play(E4_LOW, 500);
-        wait(1000);
+            // Move motor "asynchronous" to the timing
+            if (current_hr >= 24) {
+                current_hr = 0;
+            } else if (current_hr == 6) {
+                incTemp(15);
+            } else if (current_hr == 9) {
+                decTemp(3);
+            } else if (current_hr == 22) {
+                decTemp(12);
+            }
+        }
 
-        while (!PORTB);
-        if ((RB0 && is_same) || (RB1 && !is_same))
-            right++;
-        else
-            wrong++;
-        while (PORTB);
+        if (PORTB & (1 << 6)) { // RB6
+            current_hr = 0;
+            TMR2 = 0;
+            resetTMR0();
+            while (PORTB & 0x40);
+            wait(100); // Poor mans debouncer
+        }
 
-        lcd_append_all("Right: ");
-        lcd_append_int(right, 1, 0);
-        lcd_goto(1, 0);
-        lcd_append_all("Wrong: ");
-        lcd_append_int(wrong, 1, 0);
+        if (PORTB & (1 << 5)) { // RB5
+            current_hr++;
+            while (PORTB & 0x20);
+            wait(100); // Poor mans debouncer
+        }
+
+        wait(1);
     }
 }
